7a81e727e70651af71620040cb172cc04c40add5
Emanuele <Calvisi>
2020-02-13 11:50:12 +0100

    Root page + login layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RootComponent } from './root/root.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'root', 
+    component: RootComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('displayLoginForm', [
+      state('false', style({
+        
+        opacity: 0
+      })),
+      state('true', style({
+        
+        opacity: 1
+      })),
+      transition('false => true', animate('800ms')),
+      transition('true => false', animate('400ms'))
+    ]),
+    trigger('terminalPosition', [
+      state('false', style({
+        
+        bottom: '250px'
+      })),
+      state('true', style({
+        
+        bottom: '0'
+      })),
+      transition('false => true', animate('400ms')),
+      transition('true => false', animate('800ms'))
+    ]),
+  ]
 })
 export class AppComponent {
-  title = 'ngServerApp';
+
+  title = 'Angular Server App';
+  showLoginForm = 'false';
+  
+  name: string= '';
+  password: string= '';
+
+  showHideLoginForm(){
+
+    this.showLoginForm == 'false' ? this.showLoginForm = 'true' : this.showLoginForm = 'false';
+  }
+
+  submit(){
+
+    console.log("Submit ",this.name," - ",this.password)
+  }
 }
